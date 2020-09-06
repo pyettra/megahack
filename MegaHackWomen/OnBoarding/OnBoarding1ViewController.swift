2bec575e44fbf2828fe907fd636c6b86d81b03c9
Eduarda Mello <duda.mello1@hotmail.com>
2020-09-06 11:15:32 -0300

    metade das telas prontas + assets

--- MegaHackWomen/OnBoarding/OnBoarding1ViewController.swift
+++ MegaHackWomen/OnBoarding/OnBoarding1ViewController.swift
@@ -9,18 +9,33 @@
 import UIKit
 
 class OnBoarding1ViewController: UIViewController {
+    
+    @IBOutlet weak var voiceControlOutlet: UIButton!
+    @IBOutlet weak var touchOutlet: UIButton!
+    
     @IBAction func voiceControlButton(_ sender: Any) {
+//        if voiceControlOutlet.currentImage == UIImage(named: "BotaoComandodeVoz") {
+//            voiceControlOutlet.setImage(UIImage(named: "BotaoComandodeVozPreenchido"), for: .normal)
+//        } else {
+//            voiceControlOutlet.setImage(UIImage(named: "BotaoComandodeVoz"), for: .normal)
+//        }
         
     }
     
     
     @IBAction func touchButton(_ sender: Any) {
-        
+//        if touchOutlet.currentImage == UIImage(named: "BotaoToque") {
+//            touchOutlet.setImage(UIImage(named: "BotaoToquePreenchido"), for: .normal)
+//        } else {
+//            touchOutlet.setImage(UIImage(named: "BotaoToque"), for: .normal)
+//        }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        voiceControlOutlet.setImage(UIImage(named: "BotaoComandodeVoz"), for: .normal)
+//        touchOutlet.setImage(UIImage(named: "BotaoToque"), for: .normal)
 
     }
     
--- MegaHackWomen/OnBoarding/OnBoarding2ViewController.swift
+++ MegaHackWomen/OnBoarding/OnBoarding2ViewController.swift
@@ -15,43 +15,48 @@ class OnBoarding2ViewController: UIViewController {
     @IBOutlet weak var dislexiaOutlet: UIButton!
     
     @IBAction func textButton(_ sender: Any) {
-        if textOutlet.backgroundColor == nil {
-            textOutlet.backgroundColor = UIColor.yellow
+        if textOutlet.currentImage == UIImage(named: "BotaoPadrão") {
+            textOutlet.setImage(UIImage(named: "BotaoPadrãoPreenchido"), for: .normal)
         } else {
-            textOutlet.backgroundColor = nil
+            textOutlet.setImage(UIImage(named: "BotaoPadrão"), for: .normal)
         }
         
     }
     
     @IBAction func audioButton(_ sender: Any) {
-        if audioOutlet.backgroundColor == nil {
-            audioOutlet.backgroundColor = UIColor.yellow
+        if audioOutlet.currentImage == UIImage(named: "BotaoÁudio") {
+            audioOutlet.setImage(UIImage(named: "BotaoAudioPreenchido"), for: .normal)
         } else {
-            audioOutlet.backgroundColor = nil
+            audioOutlet.setImage(UIImage(named: "BotaoÁudio"), for: .normal)
         }
 
     }
     
     @IBAction func librasButton(_ sender: Any) {
-        if librasOutlet.backgroundColor == nil {
-            librasOutlet.backgroundColor = UIColor.yellow
+        if librasOutlet.currentImage == UIImage(named: "BotaoLibras") {
+            librasOutlet.setImage(UIImage(named: "BotaoLibrasPreenchido"), for: .normal)
         } else {
-            librasOutlet.backgroundColor = nil
+            librasOutlet.setImage(UIImage(named: "BotaoLibras"), for: .normal)
         }
 
     }
     
     @IBAction func dislexiaButton(_ sender: Any) {
-        if dislexiaOutlet.backgroundColor == nil {
-            dislexiaOutlet.backgroundColor = UIColor.yellow
+        if dislexiaOutlet.currentImage == UIImage(named: "BotãoDislexia") {
+            dislexiaOutlet.setImage(UIImage(named: "BotãoDislexiaPreenchido"), for: .normal)
         } else {
-            dislexiaOutlet.backgroundColor = nil
+            dislexiaOutlet.setImage(UIImage(named: "BotãoDislexia"), for: .normal)
         }
 
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textOutlet.setImage(UIImage(named: "BotaoPadrão"), for: .normal)
+        audioOutlet.setImage(UIImage(named: "BotaoÁudio"), for: .normal)
+        librasOutlet.setImage(UIImage(named: "BotaoLibras"), for: .normal)
+        dislexiaOutlet.setImage(UIImage(named: "BotãoDislexia"), for: .normal)
 
     }
 }
