2a4984e4d6f0ea7031a60dd9a0895f659e9c87e5
Eduarda Mello <duda.mello1@hotmail.com>
2020-09-07 11:42:21 -0300

    connection onboarding to home

---
+++ MegaHackWomen/Consulta/EntryHomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryHomeViewController.swift
+//  MegaHackWomen
+//
+//  Created by Eduarda Mello on 07/09/20.
+//  Copyright © 2020 Pyettra Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class EntryHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MegaHackWomen/OnBoarding/OnBoarding11ViewController.swift
+++ MegaHackWomen/OnBoarding/OnBoarding11ViewController.swift
@@ -11,12 +11,24 @@ import UIKit
 class OnBoarding11ViewController: UIViewController {
     @IBAction func startButton(_ sender: Any) {
         //chamar o app
+        let topVC = topMostController()
+        let targetStoryboard =  UIStoryboard(name: "Main", bundle: nil)
+        let vc = targetStoryboard.instantiateViewController(withIdentifier: "Home") as! EntryHomeViewController
+        vc.modalPresentationStyle = .fullScreen
+        topVC.present(vc, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     
+    }
+    func topMostController() -> UIViewController {
+    var topController: UIViewController = UIApplication.shared.keyWindow!.rootViewController!
+        while (topController.presentedViewController != nil) {
+            topController = topController.presentedViewController!
+        }
+        return topController
     }
     
 
